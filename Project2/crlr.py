ab6e2a9c2e7813c8fc61336305fd43d0b8f456b4
RecursionSheep <zy-chen17@mails.tsinghua.edu.cn>
2020-05-27 19:38:11 +0800

    crlr

--- Project2/crlr.py
+++ Project2/crlr.py
@@ -6,9 +6,9 @@ train = course_train[:, 0 : 512]
 train_context = course_train[:, 512]
 train_label = course_train[:, 513]
 size = train.shape[0]
-train = train / np.max(train, axis = 0, keep_dims = True)
+train = train / np.max(train, axis = 0, keepdims = True)
 
-binary = np.zeros(size, 512 * 4)
+binary = np.zeros((size, 512 * 4))
 for i in range(size):
 	for j in range(512):
 		for k in range(1, 5):
@@ -41,7 +41,8 @@ test_context = test_context[0 : test_cnt]
 test_label = test_label[0 : test_cnt]
 
 x_in = torch.tensor(train, dtype = torch.float)
-y_in = torch.tensor(train_label, dtype = torch.int)
+y_in = torch.tensor(train_label, dtype = torch.long)
+y_in = y_in.reshape([train_cnt, -1])
 one_hot = torch.zeros(train_cnt, 10).scatter_(1, y_in, 1)
 beta = torch.randn(2048, 10, requires_grad = True)
 w = torch.randn(train_cnt, requires_grad = True)
@@ -57,20 +58,22 @@ lambda5 = 1e-1
 lr = 0.01
 
 for treatment in range(2048):
-	x_minus = torch.copy_(x_in)
+	x_minus = x_in.clone()
 	x_minus[:, treatment] = 0.
 	bal1 = torch.matmul(x_minus.T, W * x_in[:, treatment]) / torch.matmul(W.T, x_in[:, treatment])
 	bal2 = torch.matmul(x_minus.T, W * (1 - x_in[:, treatment])) / torch.matmul(W.T, (1 - x_in[:, treatment]))
-	loss += lambda1 * (torch.norm(bal1 - bal2) ** 2)
+	loss = loss + lambda1 * (torch.norm(bal1 - bal2) ** 2)
 
-loss += lambda2 * (torch.norm(W) ** 2) + lambda3 * (torch.norm(beta) ** 2) + lambda4 * torch.norm(W, p = 1)
-loss += lambda5 * ((torch.sum(W) - 1) ** 2)
+loss = loss + lambda2 * (torch.norm(W) ** 2) + lambda3 * (torch.norm(beta) ** 2) + lambda4 * torch.norm(W, p = 1)
+loss = loss + lambda5 * ((torch.sum(W) - 1) ** 2)
 
 for it in range(100):
 	print(loss)
 	loss.backward()
 	w.sub_(w.grad.data * lr)
 	beta.sub_(beta.grad.data * lr)
+	w.grad.data.zero_()
+	beta.grad.data.zero_()
 
 x_val = torch.tensor(test, dtype = torch.float)
 pred = torch.max(torch.softmax(torch.matmul(x_val, beta), dim = 1), dim = 1)
