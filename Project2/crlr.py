3e24ed6bd4ef873feb5d3d47c159e7afbc80014a
RecursionSheep <zy-chen17@mails.tsinghua.edu.cn>
2020-05-28 14:11:18 +0800

    crlr

--- Project2/crlr.py
+++ Project2/crlr.py
@@ -3,10 +3,9 @@ import torch
 
 def forward(x_in, one_hot, w, beta):
 	W = w * w
-	print(W)
-	pred = torch.softmax(torch.matmul(x_in, beta), dim = 1)
-	loss = - (W * (one_hot * torch.log(pred)).sum(1)).sum(0)
-	lambda1 = 1e-2
+	pred = torch.clamp(torch.softmax(torch.matmul(x_in, beta), dim = 1), 1e-10, 1)
+	loss = - ((one_hot * torch.log(pred)).sum(1)).mean(0)
+	'''lambda1 = 1e-2
 	lambda2 = 1e-3
 	lambda3 = 1e-3
 	lambda4 = 1e-3
@@ -19,7 +18,7 @@ def forward(x_in, one_hot, w, beta):
 		loss = loss + lambda1 * (torch.norm(bal1 - bal2) ** 2)
 
 	loss = loss + lambda2 * (torch.norm(W) ** 2) + lambda3 * (torch.norm(beta) ** 2) + lambda4 * torch.norm(W, p = 1)
-	loss = loss + lambda5 * ((torch.sum(W) - 1) ** 2)
+	loss = loss + lambda5 * ((torch.sum(W) - 1) ** 2)'''
 	return loss
 
 course_train = np.load("course_train.npy")
@@ -68,15 +67,15 @@ one_hot = torch.zeros(train_cnt, 10).scatter_(1, y_in, 1)
 beta = torch.randn(2048, 10, requires_grad = True)
 w = torch.randn(train_cnt, requires_grad = True)
 
-lr = 0.01
+lr = 1.
 
-for it in range(100):
+for it in range(10000):
 	loss = forward(x_in, one_hot, w, beta)
-	print(loss)
+	print(loss.data)
 	loss.backward()
-	w.data = w.data - w.grad.data * lr
-	beta.data = beta.data - beta.grad.data * lr
-	w.grad.data.zero_()
+	#w.data = w.data - w.grad.data * lr
+	beta.data = beta.data - beta.grad.data * (lr / it)
+	#w.grad.data.zero_()
 	beta.grad.data.zero_()
 
 x_val = torch.tensor(test, dtype = torch.float)
